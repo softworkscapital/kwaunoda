e9b6dc7aed4c4e9985552536155b0981d2807d44
softworkscapital <devteam@softworkscapital>
2024-10-21 14:06:13 +0200

    building

--- kwaunoda/src/screens/config.js
+++ kwaunoda/src/screens/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3003';
-// export const API_URL = 'https://srv547457.hstgr.cloud:3010';
+// export const API_URL = 'http://localhost:3011';
+export const API_URL = 'https://srv547457.hstgr.cloud:3011';

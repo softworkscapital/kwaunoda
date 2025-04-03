7d17a08b9a8c54e133b2b9d247e92a40f371dbbd
Munashe <munashemudoti@gmail.com>
2025-04-03 13:25:45 +0200

    NEW UPDATE

--- xgo_backend/index.js
+++ xgo_backend/index.js
@@ -269,9 +269,17 @@ startHalfHourlyInterval();
 
 
 
+const options = {
+  cert: fs.readFileSync('/etc/letsencrypt/live/srv547457.hstgr.cloud/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/srv547457.hstgr.cloud/privkey.pem')
+};
 
-
-
-app.listen(PORT, () => {
-  console.log("app is listening to port" + " " + PORT);
+https.createServer(options, app).listen(process.env.APPPORT || '3011', () => {
+  console.log('app is listening to port' + process.env.APPPORT);
 });
+
+
+
+// app.listen(PORT, () => {
+//   console.log("app is listening to port" + " " + PORT);
+// });

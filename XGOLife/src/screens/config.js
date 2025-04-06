6ec0ea712b49b7556bb288479c3de695773e0f1f
softworkscapital <devteam@softworkscapital>
2025-04-06 03:03:20 +0200

    opened ssl

--- XGOLife/src/screens/config.js
+++ XGOLife/src/screens/config.js
@@ -1,6 +1,6 @@
-export const API_URL = "http://192.168.5.97:3011";
+//export const API_URL = "http://192.168.5.97:3011";
 // // export const API_URL_UPLOADS = "http://192.225.73.7:3020";
 // export const API_URL_STORE = "http://192.168.170.224:3009";
-// export const API_URL = 'https://srv547457.hstgr.cloud:3011';
+export const API_URL = 'https://srv547457.hstgr.cloud:3011';
 export const API_URL_STORE = "https://srv547457.hstgr.cloud:3009";
 export const API_URL_UPLOADS = "https://srv547457.hstgr.cloud:3020";
